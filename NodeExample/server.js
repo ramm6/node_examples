e0b507f8e1baab9970d67569b38199e87a121d00
Rammohan <Ram@RAM-LAPPY>
2018-08-18 22:11:38 +0530

    Initial commit for rectangle

--- NodeExample/server.js
+++ NodeExample/server.js
@@ -1,8 +1,18 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+var rect =  {
+    area: (x, y) => x * y,
+    perimeter: (x, y) => 2 * (x + y)
+};
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var solveRect = (x, y) => {
+    if (x <= 0 || y <= 0) {
+        console.log('Rectangle dimentions must be greater than zero. l = ' + x + ' and b = ' + y);
+    } else {
+        console.log('Rect area : ' + rect.area(x, y));
+        console.log('Rect perimeter : ' + rect.perimeter(x, y));
+    }
+};
+
+solveRect(2, 4);
+solveRect(9, 2);
+solveRect(0, 2);
+solveRect('a', 1);
