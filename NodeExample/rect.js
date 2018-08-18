382c5773ab76328db43ae32097debff9f9abc237
Rammohan <Ram@RAM-LAPPY>
2018-08-19 00:11:36 +0530

    Node module implemented

---
+++ NodeExample/rect.js
@@ -0,0 +1,2 @@
+module.exports.area = (x, y) => x * y;
+module.exports.perimeter = (x, y) => 2 * (x + y);
--- NodeExample/server.js
+++ NodeExample/server.js
@@ -1,15 +1,17 @@
-var rect =  {
-    area: (x, y) => x * y,
-    perimeter: (x, y) => 2 * (x + y)
-};
-
+var rect = require('./rect');
+//{
+//    area: (x, y) => x * y,
+//    perimeter: (x, y) => 2 * (x + y)
+//};
 var solveRect = (x, y) => {
+
     if (x <= 0 || y <= 0) {
         console.log('Rectangle dimentions must be greater than zero. l = ' + x + ' and b = ' + y);
     } else {
         console.log('Rect area : ' + rect.area(x, y));
         console.log('Rect perimeter : ' + rect.perimeter(x, y));
     }
+
 };
 
 solveRect(2, 4);
