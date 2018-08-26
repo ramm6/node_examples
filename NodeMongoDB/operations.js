4edd5eade03d7c44e86738a2cf575d53c0fb0afd
Rammohan <Ram@RAM-LAPPY>
2018-08-26 12:18:11 +0530

    Node callback hell and promices implemented

--- NodeMongoDB/operations.js
+++ NodeMongoDB/operations.js
@@ -1,34 +1,17 @@
 ﻿const assert = require('assert');
 
 exports.insertDocument = (db, document, collection, callback) => {
-    //const coll = db.collection(collection);
-    collection.insert(document, (err, result) => {
-        assert.equal(err, null);
-        console.log('Inserted ' + result.result.n + ' documents into collection ' + collection);
-        callback(result);
-    });
+    return collection.insertOne(document);
 };
 
 exports.findDocuments = (db, collection, callback) => {
-    //const coll = db.collection(collection);
-    collection.find({}).toArray((err, docs) => {
-        assert.equal(err, null);
-        callback(docs);
-    })
+    return collection.find({}).toArray();
 };
 
 exports.updateDocument = (db, document, update, collection, callback) => {
-    //const coll = db.collection(collection);
-    collection.updateOne(document, { $set: update }, null, (err, result) => {
-        assert.equal(err, null);
-        callback(result);
-    });
+    return collection.updateOne(document, {$set : update}, null);
 };
 
 exports.deleteDocument = (db, document, collection, callback) => {
-    //const coll = db.collection(collection);
-    collection.deleteOne(document, (err, result) => {
-        console.log('Deleted document :', document);
-        callback(result);
-    });
+    return collection.deleteOne(document);
 };
--- NodeMongoDB/server.js
+++ NodeMongoDB/server.js
@@ -5,35 +5,39 @@ const operations = require('./operations');
 var server = 'mongodb://testuser:password@ds133558.mlab.com:33558/testapp';
 const dbname = 'testapp';
 
-mongoClient.connect(server, (err, client) => {
+mongoClient.connect(server).then((client) => {
     
-    assert.equal(err, null);
-
     console.log('Connected to server successfully.');
     const db = client.db(dbname);
     const collection = db.collection('dishes');
 
-    operations.insertDocument(db, { "name": "rammohan", "description": "testing from coursera method." }, collection,
-        (result) => {
+    //Callback hell is implemented.
 
-            assert.equal(err, null);
-            console.log('After insert');
-            console.log(result.ops);
-
-            operations.findDocuments(db, collection, (docs) => {
-                console.log('Found: \n');
-                console.log(docs);
-                operations.updateDocument(db, { name: "rammohan" },
-                    { "education": "graduate" }, collection, (result) => {
-                        console.log("Updated document :", result.result);
-                    operations.findDocuments(db, collection, (docs) => {
-                        console.log("Found documents : \n", docs);
-                        db.dropCollection('dishes', (res) => {
-                            client.close();
-                        });
-                    });
-                });
-            });
-
+    operations.insertDocument(db, { "name": "rammohan1", "description": "testing from coursera method." }, collection)
+        .then((result) => {
+            console.log('Inserted document : \n' + JSON.stringify(result.ops) + '\n');
+            return operations.findDocuments(db, collection);
+        })
+        .then((docs) => {
+            console.log('Found: \n' + JSON.stringify(docs) + '\n');
+            return operations.updateDocument(db, { "name": "rammohan1" },
+                    { "education": "graduate" }, collection);
+        })
+        .then((result) => {
+            console.log("Updated document :", JSON.stringify(result.result) + '\n');
+            return operations.findDocuments(db, collection);
+        })
+        .then((docs) => {
+            console.log("Found documents : \n", JSON.stringify(docs));
+            return db.dropCollection('dishes');
+        })
+        .then((res) => {
+            client.close();
+        })
+        .catch((error) => {
+            console.log("Error : " + error)
         });
-});
+    })
+    .catch((error) => { 
+        console.log("Error : " + error)
+     });
